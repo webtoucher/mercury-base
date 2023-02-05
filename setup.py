6b6b8f2ef45915fb3ec4c5f853052ecb31453418
Алексей Кузнецов <mirakuru@webtoucher.ru>
2023-02-06 02:01:54 +0700

    Update socket server version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mercury-base',
-      version='1.1',
+      version='1.2',
       url='https://github.com/webtoucher/mercury-base',
       license='BSD-3-Clause',
       author='Alexey Kuznetsov',
@@ -21,6 +21,6 @@ setup(name='mercury-base',
       install_requires=[
           'modbus-crc~=1.3',
           'pyserial~=3.0',
-          'simple-socket-client~=1.3',
+          'simple-socket-client~=1.4',
       ],
       zip_safe=False)
